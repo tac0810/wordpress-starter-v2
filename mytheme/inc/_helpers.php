c8dd33e4266ac5df689e0a06b6e4e51c89a89f7b
Calamarify <16755396+tac0810@users.noreply.github.com>
2024-04-16 23:43:53 +0900

    Update _helpers.php

--- mytheme/inc/_helpers.php
+++ mytheme/inc/_helpers.php
@@ -42,3 +42,22 @@ function renderTemplates($templates, $context)
 		echo $render;
 	}
 }
+
+function checkViteConnection()
+{
+
+	if (!$_ENV['IS_DEVELOPMENT']) {
+		return false;
+	}
+
+	$host = 'host.docker.internal';
+	$port = 3000;
+	$connection = @fsockopen($host, $port, $errno, $errstr, 5); // 5秒のタイムアウト
+
+	if ($connection) {
+		fclose($connection);
+		return true;
+	} else {
+		return false;
+	}
+}
