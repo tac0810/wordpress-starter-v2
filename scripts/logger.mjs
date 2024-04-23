45f6552a823e793a000372ce69afb9246c43b47f
s.takaaki <16755396+tac0810@users.noreply.github.com>
2024-04-23 21:56:02 +0900

    update logger.mjs

--- scripts/logger.mjs
+++ scripts/logger.mjs
@@ -1,17 +1,37 @@
-import { Tail } from "tail";
+import fs from "fs/promises";
 import path from "path";
+import { Tail } from "tail";
 import { config } from "dotenv";
 
 config();
 
 const root = path.dirname(path.dirname(new URL(import.meta.url).pathname));
 const filePath = path.resolve(root, process.env.THEME_NAME, "my-errors.log");
-const tail = new Tail(filePath);
 
-tail.on("line", (data) => {
-  console.log(data);
-});
+// ファイルの内容を消去する関数
+async function clearFileContents(filePath) {
+  try {
+    await fs.writeFile(filePath, ''); // 空の内容で上書きする
+    console.log(`ファイル ${filePath} の内容を消去しました。`);
+  } catch (error) {
+    console.error("ファイルの内容の消去中にエラーが発生しました:", error);
+  }
+}
 
-tail.on("error", (error) => {
-  console.error("Error:", error);
+// my-errors.logファイルの内容を消去
+clearFileContents(filePath)
+.then(() => {
+  // tailを開始
+  const tail = new Tail(filePath);
+
+  tail.on("line", (data) => {
+    console.log(data);
+  });
+
+  tail.on("error", (error) => {
+    console.error("Error:", error);
+  });
+})
+.catch((error) => {
+  console.error("ファイルの内容の消去中にエラーが発生しました:", error);
 });
